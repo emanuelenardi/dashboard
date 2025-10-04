802ef4ed91264132a87ba16d4c691abd7015a287
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-10-04 03:12:12 +0200

    feat: add decks statistics page with dashboard panel and navbar structure

---
+++ app/pages/statistics/decks.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <UDashboardPanel id="decks">
+    <template #header>
+      <UDashboardNavbar title="Decks">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+
+        <template #right>
+          <EventsAddModal />
+        </template>
+      </UDashboardNavbar>
+
+      <UDashboardToolbar>
+        <template #left>
+          <!-- NOTE: The `-ms-1` class is used to align with the `DashboardSidebarCollapse` button here. -->
+          <!-- <HomeDateRangePicker v-model="range" class="-ms-1" /> -->
+        </template>
+      </UDashboardToolbar>
+    </template>
+  </UDashboardPanel>
+</template>
